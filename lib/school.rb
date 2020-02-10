6ec18b89c6267f02d169f301ace056b7aa81c43a
Kasey Arnold <kaseyarnold15@gmail.com>
2020-02-10 02:40:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,11 @@ school.roster
 school.add_student ("Zach Morris", 9)
 school.roster
 
+school.add_student ("AC Slater", 9)
+school.add_student ("Kelly Kapowski", 10)
+school.add_student ("Screech", 11)
+school.roster
+
 hash = {}
 hash["new_key"] = []
 
