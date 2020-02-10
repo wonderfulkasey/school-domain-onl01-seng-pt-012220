d7f4d43e1c8d8bc869038d4da6b1d2d54233b1dc
Kasey Arnold <kaseyarnold15@gmail.com>
2020-02-10 02:40:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,20 +1,2 @@
 # code here!
 
-school = School.new("Bayside High School")
-
-school.roster
-
-school.add_student ("Zach Morris", 9)
-school.roster
-
-school.add_student ("AC Slater", 9)
-school.add_student ("Kelly Kapowski", 10)
-school.add_student ("Screech", 11)
-school.roster
-
-hash = {}
-hash["new_key"] = []
-
-hash["new_key"] << "new_value_for_value_array"
-
-hash => {"new_key"=>["new_value_for_value_array"]}
